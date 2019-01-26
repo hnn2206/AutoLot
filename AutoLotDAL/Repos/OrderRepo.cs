1c9c37c4bbc463cb3b9379144f4fcfabb90bc852
Hoang Nguyen <hnn2206@gmail.com>
2019-01-25 17:24:13 -0800

    Add order repo

---
+++ AutoLotDAL/Repos/OrderRepo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoLotDAL.Models;
+
+namespace AutoLotDAL.Repos
+{
+    class OrderRepo : BaseRepo<Order>, IRepo<Order>
+    {
+        public int Delete(int id)
+        {
+            Context.Entry(new Order() { CarId = id }).State = EntityState.Deleted;
+            return SaveChanges();
+        }
+
+        public Task<int> DeleteAsync(int id)
+        {
+            Context.Entry(new Order() { CarId = id }).State = EntityState.Deleted;
+            return SaveChangesAsync();
+        }
+    }
+}
